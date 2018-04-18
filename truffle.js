adeb0cceaba8a486b556ac1a28c4270f3fbb0734
K Systems <info@ksystems.io>
2018-04-18 11:05:25 +0200

    Added files via upload
    Master branch, node_modules must be installed locally

---
+++ truffle.js
@@ -0,0 +1,13 @@
+module.exports = {
+  networks: {
+kovan:{
+ 
+from: '0x1d1D0F00194275DB6911f54c3aA1eCd1c799337d',
+network_id: 42,
+host:'localhost',
+port:8545,
+gas: 7712388,
+gasPrice: 30000000000
+}
+  }
+};
